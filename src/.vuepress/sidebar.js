821f786da503ac35e3de228582a49d2b40abaaca
是一只栗子啊 <chairowell@gmail.com>
2025-03-12 01:12:53 +0800

    Revert "⚙️update(config): 更新配置"
    This reverts commit cae12f5f0520758aa188ac0ad1fad8a00165dc13.

--- src/.vuepress/sidebar.js
+++ src/.vuepress/sidebar.js
@@ -1,11 +1,5 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-    "/vue/": [
-        {
-        //   text: "Vue",
-        //   icon: "logos:vue",
-          children: "structure",
-        }
-    ],
+
 });
