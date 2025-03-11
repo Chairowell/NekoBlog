dc34e8928f1a0c1a607647fa401664627677f3fe
是一只栗子啊 <chairowell@gmail.com>
2025-03-12 00:52:29 +0800

    ✨ feat(theme): 开启部分文章侧边栏

--- src/.vuepress/sidebar.js
+++ src/.vuepress/sidebar.js
@@ -1,5 +1,11 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-
+    "/vue/": [
+        {
+        //   text: "Vue",
+        //   icon: "logos:vue",
+          children: "structure",
+        }
+    ],
 });
