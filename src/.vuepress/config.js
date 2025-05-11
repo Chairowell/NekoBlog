21cd0a1651f105a5392b19432e19328876139dd5
chairowell <chairowell@gmail.com>
2025-05-11 15:12:19 +0800

    ✨ feat(config): 添加meting2插件
    插件：https://github.com/OrageKK/vuepress-plugin-meting2
    MetingAPI：https://qijieya.cn/archives/700

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
-
 import theme from "./theme.js";
+import metingPlugin from "vuepress-plugin-meting2";
 
 export default defineUserConfig({
   base: "/",
@@ -13,4 +13,27 @@ export default defineUserConfig({
 
   // 和 PWA 一起启用
   shouldPrefetch: false,
+
+  plugins: [
+    metingPlugin({
+      metingOptions: {
+        global:true,
+        api: "https://api.qijieya.cn/meting/?server=:server&type=:type&id=:id&auth=:auth&r=:r",
+        server: "netease",
+        type: "playlist",
+        mid: "6846157420",
+        aplayerOptions: {
+          fixed: true,
+          mini: true,
+          autoplay: false,
+          theme: "#a33535",
+          loop: "all",
+          order: "random",
+          preload: "auto",
+          volume: 0.5,
+          mutex: true,
+        }
+      },
+    }),
+  ],
 });
